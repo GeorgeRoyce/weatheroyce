3d74664d3e070a81dc522b18bf1bea73f7d2e033
George Royce <george.royce@detroitlabs.com>
2018-05-10 14:30:24 -0400

    Initial Commit

---
+++ weatheroyce/ViewControllers/CurrentWeatherCondiditions.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeatherCondiditions.swift
+//  weatheroyce
+//
+//  Created by George Royce on 5/10/18.
+//  Copyright © 2018 George Royce. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherCondiditions: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
