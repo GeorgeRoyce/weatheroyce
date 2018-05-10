8b450410debbfed391113bf79dc9669342957e08
George Royce <george.royce@detroitlabs.com>
2018-05-10 16:06:49 -0400

    create model for showing current conditions.
    The only current condition needed at this time is temperature in degrees fahrenheit

---
+++ weatheroyce/Models/CurrentWeatherConditions.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeatherConditions.swift
+//  weatheroyce
+//
+//  Created by George Royce on 5/10/18.
+//  Copyright © 2018 George Royce. All rights reserved.
+//
+
+import Foundation
